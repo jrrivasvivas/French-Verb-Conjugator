3a149b78d2ea5b5504bd72f920aa5b9380693873
jrrivasvivas <jrrivasvivas@gmail.com>
2023-11-04 16:25:33 -0400

    French menu

--- main.py
+++ main.py
@@ -4,42 +4,56 @@ from src.logger import ErrorLogger  # Importar ErrorLogger desde logger.py en la
 
 class ConjugationGame:
     def __init__(self):
+        # Initialize Conjugator and ErrorLogger objects
         self.conjugator = Conjugator()
-        self.logger = ErrorLogger("2023-11")  # Crear un objeto ErrorLogger para el mes actual
+        self.logger = ErrorLogger("2023-11")  # Create an ErrorLogger object for the current month
         self.score = {'correct': 0, 'incorrect': 0}
 
     def play_round(self):
+        # Generate a random verb and prompt user input
         verb, pronoun, tense = self.conjugator.generate_random_verb()
+        print(f"Conjuguez le verbe '{verb}' à '{pronoun}' au '{tense}'.")
 
-        print(f"Conjugue el verbo '{verb}' en '{pronoun}' en '{tense}'.")
+        user_input = input("Entrez la conjugaison (pronom verbe) : ")
 
-        user_input = input("Ingrese la conjugación (pronombre verbo): ")
-        user_pronoun, user_verb = user_input.split()
+        # Split the input into words
+        words = user_input.split()
+
+        # Verify if there are enough words (at least two) in the input
+        if len(words) >= 2:
+            user_pronoun = words[0]  # The first word is the pronoun
+            user_verb = ' '.join(words[1:])  # The rest of the words are the verb (even if it contains spaces)
+        else:
+            print("Entrée invalide. Doit inclure au moins un pronom et un verbe.")
+            return
 
         correct_conjugation = self.conjugator.conjugate(verb, pronoun, tense)
 
         if user_verb == correct_conjugation and user_pronoun == pronoun:
-            print("¡Correcto!")
+            print("Correct !")
             self.score['correct'] += 1
-            result = 'Correcto'
+            result = 'Correct'
         else:
-            print(f"Incorrecto. La conjugación correcta es: {pronoun} {correct_conjugation}")
+            print(f"Incorrect. La conjugaison correcte est : {pronoun} {correct_conjugation}")
             self.score['incorrect'] += 1
-            result = 'Incorrecto'
+            result = 'Incorrect'
 
-        # Registra la respuesta en el archivo de log
         self.logger.log_error(pronoun, verb, tense, user_verb, correct_conjugation, result)
 
-
     def start_game(self):
-        print("¡Bienvenido al juego de conju gación de verbos!")
+        print("Bienvenue au jeu de conjugaison des verbes !")
         while True:
             self.play_round()
-            print(f"Aciertos: {self.score['correct']} - Errores: {self.score['incorrect']}")
-            play_again = input("¿Quieres jugar de nuevo? (s/n): ").lower()
-            if play_again != 's':
-                print("¡Gracias por jugar! Hasta luego.")
+            print(f"Réussites : {self.score['correct']} - Erreurs : {self.score['incorrect']}")
+            play_again = input("Voulez-vous jouer à nouveau ? (o/n) : ").lower()
+            if play_again != 'o':
+                print("Merci d'avoir joué ! Au revoir.")
                 break
+
+if __name__ == "__main__":
+    game = ConjugationGame()
+    game.start_game()
+
 
 if __name__ == "__main__":
     game = ConjugationGame()
